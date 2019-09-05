0f2d74688a72d247a5d1c350585f9cb600fbb2a3
Dmitriy <mityacrazzy@gmail.com>
2019-09-06 00:46:32 +0300

    Change front-end

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -7,10 +7,11 @@ class Chat extends Component {
         super(props);
 
         this.state = {
-            messages: []
+            messages: [],
         };
-        const {userName} = this.props;
-        this.socket = socketIOClient(`http://localhost?user=${userName}&chatRoom=chat`);
+        this.chatRoomId = 1; // default room after enter to chat
+        this.chatRooms = [{name: "Чат 1", id: 1}, {name: "Чат 2", id: 2}, {name: "Чат 3", id: 3}];
+        this.socket = socketIOClient(`http://localhost`);
     }
 
     componentDidMount() {
@@ -19,36 +20,59 @@ class Chat extends Component {
             const messages = [...state.messages, message];
             return {...state, messages: messages};
         }));
-    }
 
+        this.socket.emit('join', {chatRoomId: this.chatRoomId, user: this.props.userName});
+        this.getRoomMessages();
+    }
 
     render() {
         const messages = this.state.messages.map((message, index) => {
-            return <div key={index}>{message}</div>
+            return <div key={index}>{message.message} - {message.user}</div>
         });
-
+        const chatList = this.chatRooms.map(chatRoom => {
+            return <div key={chatRoom.id} onClick={() => this.changeRoom(chatRoom.id)}>{chatRoom.name}</div>
+        });
         return (
-            <div>
-                <div className="Chat">
-                    {messages}
+            <div className="ChatsContent">
+                <div className="ChatList">
+                    {chatList}
                 </div>
-                <textarea onKeyPress={this.sendMessage}/>
+                <div>
+                    <div className="Chat">
+                        {messages}
+                    </div>
+                    <textarea onKeyPress={this.sendMessage}/>
+                </div>
             </div>
         )
     }
 
     sendMessage = (event) => {
+        const {userName} = this.props;
+
         if(event.key === 'Enter') {
-            const message = event.target.value;
+            const message = {message: event.target.value, user: userName, chatRoomId: this.chatRoomId};
             event.target.value = ''; // clear text field?????
 
             this.setState(state => {
                 const messages = [...state.messages, message];
-                return {...state, messages: messages, currentValue: ''};
+                return {...state, messages: messages};
             });
 
             this.socket.emit('message', message);
         }
+    };
+
+    getRoomMessages() {
+        this.socket.emit('getMessages', this.chatRoomId);
+        this.socket.on('getMessages', messages => this.setState(state => {
+            return {...state, messages: messages};
+        }))
+    }
+
+    changeRoom = (chatRoomId) => {
+        this.chatRoomId = chatRoomId;
+        this.getRoomMessages();
     }
 }
 
